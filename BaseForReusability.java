5aa32bfd3d4d09a34eb547dd600c2da2ca3dad5e
Vikas <vicky.vikas178@gmail.com>
2016-07-14 14:27:05 +0530

    commit

---
+++ BaseForReusability.java
@@ -0,0 +1,30 @@
+package com.ne.PractiseFramework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseForReusability implements UrlInterface {
+	
+	public WebDriver driver ;
+	
+	public WebDriver initialization (String sBrowser)
+	{
+		if(sBrowser.equalsIgnoreCase("Firefox"))
+		{
+			driver = new FirefoxDriver();
+		}else if(sBrowser.equalsIgnoreCase("Chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver","E:/softwares/Browsers/chromedriver.exe");
+			System.oyt.println("changing for test");
+			driver = new ChromeDriver();
+		}
+		driver.manage().window().maximize();
+		return driver;		
+	}
+	public void launchBrowser()
+	{
+		driver.get(URL);
+	}
+
+}
---
+++ BaseForReusability3.java
@@ -0,0 +1,29 @@
+package com.ne.PractiseFramework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseForReusability implements UrlInterface {
+	
+	public WebDriver driver ;
+	
+	public WebDriver initialization (String sBrowser)
+	{
+		if(sBrowser.equalsIgnoreCase("Firefox"))
+		{
+			driver = new FirefoxDriver();
+		}else if(sBrowser.equalsIgnoreCase("Chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver","E:/softwares/Browsers/chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+		driver.manage().window().maximize();
+		return driver;		
+	}
+	public void launchBrowser()
+	{
+		driver.get(URL);
+	}
+
+}
